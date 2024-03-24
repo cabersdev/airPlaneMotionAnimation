688a630d859b063eb44030bc62ef65484b571d5c
cabersdev <caber@cabers-MBP.station>
2024-03-24 21:27:38 +0100

    Upd Animation class

--- src/animation/animation.py
+++ src/animation/animation.py
@@ -23,17 +23,26 @@ class Animation:
         # Subplot 1
         self.ax0 = self.fig.add_subplot(self.gs[0, :], facecolor=(0.9, 0.9, 0.9))
         self.plane_trajectory, = self.ax0.plot([], [], 'r:o', linewidth=2)
-        # Add other plot elements here...
+        self.ax0.set_xlim(min(self.x), max(self.x))
+        self.ax0.set_ylim(0, max(self.y) * 1.1)  # Adjust y-limit to show the entire trajectory
+        self.ax0.set_xlabel('Distance (km)')
+        self.ax0.set_ylabel('Altitude (km)')
 
         # Subplot 2
         self.ax2 = self.fig.add_subplot(self.gs[1, 0], facecolor=(0.9, 0.9, 0.9))
         self.x_dist, = self.ax2.plot([], [], '-b', linewidth=3, label='X=800*t')
-        # Add other plot elements here...
+        self.ax2.set_xlim(min(self.x), max(self.x))
+        self.ax2.set_ylim(0, max(self.y) * 1.1)  # Adjust y-limit to show the entire trajectory
+        self.ax2.set_xlabel('Time (hrs)')
+        self.ax2.set_ylabel('Distance (km)')
 
         # Subplot 3
         self.ax4 = self.fig.add_subplot(self.gs[1, 1], facecolor=(0.9, 0.9, 0.9))
         self.speed, = self.ax4.plot([], [], '-b', linewidth=3, label='ΔX/Δt = 800')
-        # Add other plot elements here...
+        self.ax4.set_xlim(min(self.x), max(self.x))
+        self.ax4.set_ylim(0, self.speed_x * 1.1)  # Adjust y-limit to show the entire speed range
+        self.ax4.set_xlabel('Time (hrs)')
+        self.ax4.set_ylabel('Speed (km/h)')
 
         # Initialize the animation
         self.plane_ani = animation.FuncAnimation(self.fig, self.update_plot,
@@ -44,9 +53,10 @@ class Animation:
         plt.show()
 
     def update_plot(self, num):
-        # Update plot elements here...
-        # Example: updating the plane_trajectory plot
+        # Update plot elements
         self.plane_trajectory.set_data(self.x[:num], self.y[:num])
-        
+        self.x_dist.set_data(self.flight.t[:num], self.x[:num])
+        self.speed.set_data(self.flight.t[:num], [self.speed_x] * num)  # Constant speed for all time
+
         # Return the updated plot elements as a sequence of Artist objects
-        return self.plane_trajectory,
+        return self.plane_trajectory, self.x_dist, self.speed
